d729be2ef6a8f9cb5ed3afec9fa050020228fb56
Tuo Nome <double.facessss@gmail.com>
2024-01-16 20:21:49 +0100

    fixing grid header, set package.json

--- components/welcomeComp.vue
+++ components/welcomeComp.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-      <div class="text-center">
-        <h1 class="text-4xl font-bold mb-4">Benvenuto su Nuxt.js con Tailwind CSS</h1>
-        <p class="text-gray-600">Una pagina Home minimale e carina.</p>
-      </div>
+  <div class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold mb-4">
+        Benvenuto su Nuxt.js con Tailwind CSS
+      </h1>
+      <p class="text-gray-600">Una pagina Home minimale e carina.</p>
     </div>
-  </template>
+  </div>
+</template>
   
   <style scoped>
-  </style>
+</style>
   
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <WelcomeComp />
+  <div class="container flex flex-col justify-center items-center min-h-screen">
+    <div class="col text-center mb-4">
+      <p>header</p>
+      <p>1 of 1</p>
+    </div>
+    <div class="row"> <!-- Aggiunta della classe justify-center -->
+      <WelcomeComp />
+    </div>
+    <div class="row">
+      <div class="col text-center mt-4">
+        <p>footer</p>
+        <p>1 of 1</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
