6c0d8d61a0460ce0ffd9020255f563c529a848ac
Tuo Nome <double.facessss@gmail.com>
2024-01-16 20:23:42 +0100

    fixing grid header

--- components/welcomeComp.vue
+++ components/welcomeComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-gray-100">
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="text-center">
       <h1 class="text-4xl font-bold mb-4">
         Benvenuto su Nuxt.js con Tailwind CSS
