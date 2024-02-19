ddf79aafedadf5f7bdee7dc6b4295942b8ba0db9
Tuo Nome <double.facessss@gmail.com>
2024-02-19 18:23:29 +0100

    migrating prod site config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { 
+    enabled: true 
+  },
+  components: true,
+  runtimeConfig: {
+    apiKey: process.env.NUXT_API_KEY || 'super-secret-key',
+    // Warning: `public` is exposed on client and server
+    public: {
+      websiteURL: 'https://public-data.com'
+    }
+  },
   css: [
     '@/assets/css/main.css',
   ],
