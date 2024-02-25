09a989e277afb6022b73f3d49362dec412d5bf9c
Tuo Nome <double.facessss@gmail.com>
2024-02-25 10:04:40 +0100

    added hero

--- components/hero.vue
+++ components/hero.vue
@@ -1,65 +1,61 @@
 <script>
 //import searchBar from '~/components/home/hero/search.vue'
-import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from "vue";
 
 export default {
-  name: 'hero',
+  name: "hero",
   components: {
     //searchBar
   },
   setup() {
-    const windowWidth = ref(0)
+    const windowWidth = ref(0);
 
     const getImage = () => {
-      windowWidth.value = window.innerWidth
+      windowWidth.value = window.innerWidth;
+      const imgPath = "localhost:3000/img";
       if (windowWidth.value <= 720) {
-        return require("@/assets/img/hero-bg@480x334.png");
+        return `/${imgPath}hero-bg@480x334.png`;
       } else if (windowWidth.value > 720 && windowWidth.value <= 1080) {
-        return require("@/assets/img/hero-bg@1680x748.png");
+        return `/${imgPath}hero-bg@1680x748.png`;
       } else {
-        return require("@/assets/img/hero-bg.png");
+        return `/${imgPath}hero-bg.png`;
       }
-    }
+    };
 
     const _getImage = () => {
-      return getImage()
-    }
+      return getImage();
+    };
 
     onMounted(() => {
       window.addEventListener("resize", _getImage);
-    })
+    });
 
     onBeforeUnmount(() => {
       window.removeEventListener("resize", _getImage);
-    })
+    });
 
     return {
       windowWidth,
-      getImage
-    }
-  }
-}
+      getImage,
+    };
+  },
+};
 </script>
 <template>
-    <section class="container-fluid my-5 pt-5 pb-lg-4 px-xxl-4">
-      <!--
-      <div 
-        class="jarallax card align-items-center justify-content-center border-0 p-md-5 p-4 bg-secondary overflow-hidden mt-n3" 
-        style="min-height: 65vh; 
-        background-image: url(https://www.difalcoimmobiliare.it/img/hero-bg.png);" 
-        data-jarallax data-speed="0.5"
-      >
-      -->
-      <div 
-        class="jarallax card align-items-center justify-content-center border-0 p-md-5 p-4 bg-secondary overflow-hidden mt-n3" 
-        :style="{ 'background-image': `url(${getImage})`, 'min-height': '65vh' }" 
-        data-jarallax data-speed="0.5"
-      >
-        <span class="img-overlay opacity-40"></span>
-        <div class="content-overlay" style="max-width: 856px;">
-          <h1 class="display-5 mb-5 pb-md-3 px-md-3 text-white text-center">Immobiliare di Falco</h1>      
-          <!--<searchBar />-->
-        </div>
+  <section class="container-fluid my-5 pt-5 pb-lg-4 px-xxl-4">
+    <div
+      class="jarallax card align-items-center justify-content-center border-0 p-md-5 p-4 bg-secondary overflow-hidden mt-n3"
+      :style="{ 'background-image': `url(${getImage})`, 'min-height': '65vh' }"
+      data-jarallax
+      data-speed="0.5"
+    >
+      <span class="img-overlay opacity-40"></span>
+      <div class="content-overlay" style="max-width: 856px">
+        <h1 class="display-5 mb-5 pb-md-3 px-md-3 text-white text-center">
+          Immobiliare di Falco
+        </h1>
+        <!--<searchBar />-->
       </div>
-    </section>
-    </template>
+    </div>
+  </section>
+</template>
