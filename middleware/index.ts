d5e017ccb024161cd8b446b012a6438340ac2c36
Tuo Nome <double.facessss@gmail.com>
2024-03-03 12:23:33 +0100

    testing middleware and pinia store

---
+++ middleware/index.ts
@@ -0,0 +1,9 @@
+/*import { useHomeStore } from '~/store/main'
+
+const homeStore = useHomeStore()
+export default function({ 
+    homeStore, 
+    route 
+}) {  store.commit('SetClass', route.name)}
+
+*/
--- store/main.js
+++ store/main.js
@@ -3,7 +3,20 @@ import { defineStore } from 'pinia';
 export const useHomeStore = defineStore('home', {
   state: () => ({
     title: 'Benvenuto su Nuxt.js con Tailwind CSS',
-    text: 'Benvenuto su Nuxt.js con Tailwind CSS'
+    text: 'Benvenuto su Nuxt.js con Tailwind CSS',
+    /* is test */
+    bodyClass: '',
+    application: {
+      status: 'idle',
+      code: 0,
+      error: false,
+      invoker: false,
+    }
   }),
-});
+  actions: {
+    SetClass(bodyClass) {
+      this.bodyClass = bodyClass  
+    }
+  }
+})
 
